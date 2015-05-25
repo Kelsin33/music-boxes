a824f4bfc7386bad676c838342a56992a0588db9
Kellee Massey <kdmassey2@gmail.com>
2015-05-25 18:07:17 -0400

    initial commit

---
+++ JS/scripts.js
@@ -0,0 +1,16 @@
+	var cNote = document.getElementById('cAudio');
+	var dNote = document.getElementById('dAudio');
+	var eNote = document.getElementById('eAudio');
+	var fNote = document.getElementById('fAudio');
+	var gNote = document.getElementById('gAudio');
+	var aNote = document.getElementById('aAudio');
+	var bNote = document.getElementById('bAudio');
+
+	$('#c').click(function(){
+		cNote.currentTime = 0;
+		cNote.play();
+	});
+
+	$('#d').click(function(){
+		dnote.Play();
+	})
