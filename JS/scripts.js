c6c02fb50d2ca5ab6bb9a3e020f528a730bf18bd
Kellee Massey <kdmassey2@gmail.com>
2015-05-25 18:34:27 -0400

    added mouseover sounds

--- JS/scripts.js
+++ JS/scripts.js
@@ -8,37 +8,37 @@ $(document).ready( function() {
 	var aNote = document.getElementById('aAudio');
 	var bNote = document.getElementById('bAudio');
 
-	$('#c').click(function(){
+	$('#c').mouseover(function(){
 		cNote.currentTime = 0;
 		cNote.play();
 	});
 
-	$('#d').click(function(){
+	$('#d').mouseover(function(){
 		dNote.currentTime = 0;
 		dNote.play();
 	});
 
-	$('#e').click(function(){
+	$('#e').mouseover(function(){
 		eNote.currentTime = 0;
 		eNote.play();
 	});
 
-	$('#f').click(function(){
+	$('#f').mouseover(function(){
 		fNote.currentTime = 0;
 		fNote.play();
 	});
 
-	$('#g').click(function(){
+	$('#g').mouseover(function(){
 		gNote.currentTime = 0;
 		gNote.play();
 	});
 
-	$('#a').click(function(){
+	$('#a').mouseover(function(){
 		aNote.currentTime = 0;
 		aNote.play();
 	});
 
-	$('#b').click(function(){
+	$('#b').mouseover(function(){
 		bNote.currentTime = 0;
 		bNote.play();
 	});
